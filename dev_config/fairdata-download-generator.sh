e4cf8f32246e077cbc66f71317192291b7afd90f
Patrick Stickler <stickler@csc.fi>
2022-11-01 12:31:16 +0000

    Updates to configuration and tests to be compatible with AlmaLinux9 Docker environment
    Python virtual environment requirements updated to latest versions which are compatible both with Centos7 and AlmaLinux9

--- dev_config/fairdata-download-generator.sh
+++ dev_config/fairdata-download-generator.sh
@@ -28,4 +28,5 @@ source venv/bin/activate
 
 ${CELERY_BIN} multi start ${CELERYD_NODES} \
     -A ${CELERY_APP} --pidfile=${CELERYD_PID_FILE} \
-    --logfile=${CELERYD_LOG_FILE} --loglevel=${CELERYD_LOG_LEVEL} ${CELERYD_OPTS}
+    --logfile=${CELERYD_LOG_FILE} --loglevel=${CELERYD_LOG_LEVEL} \
+    ${CELERYD_OPTS}
